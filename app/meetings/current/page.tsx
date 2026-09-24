4653b4da4893206763704eac256a54784f420985
elitefx75 <elitexystus5@gmail.com>
2026-09-24 12:33:14 +0300

    Sacrament meeting planner assignment

--- app/meetings/current/page.tsx
+++ app/meetings/current/page.tsx
@@ -1,6 +1,8 @@
 import { redirect } from "next/navigation";
 import { getMeetings } from "../../../lib/meetings-db";
 
+export const dynamic = "force-dynamic";
+
 export default function CurrentMeetingPage() {
     const today = new Date();
     const sunday = new Date(today);
